af39dea99163d80adea8866607eafadb8d86b23b
ts <trym.nilsen@gmail.com>
2015-05-07 21:45:00 +0200

    Added app state

--- ts/AppEntryPoint.ts
+++ ts/AppEntryPoint.ts
@@ -1,21 +1,29 @@
 /// <reference path="core/Application.ts" />
-
+var t0 = performance.now();
+console.log('App Start at '+t0+'ms');
 /**
  * Run on Dom Ready
  */
 $(() => {
- 	var t0 = performance.now();
-    console.log('App Start at '+t0+'ms');
-	/**
-	 * Holds the reference to our application object
-	 */
-	var mainApp : App.Application = new App.Application();
-	/**
-	 * Calls startup methods and dependencies in the Application
-	 */
-    mainApp.bootstrap();
+ 	var t1 = performance.now();
+    console.log('Dom Start at '+t1+'ms');
+    console.log('DomReady took '+(t1 - t0)+' milliseconds');
+    setTimeout(function() {
+        var t2 = performance.now();
+        console.log('timeout at '+t2+'ms');
+        /**
+         * Holds the reference to our application object
+         */
+        var mainApp : App.Application = new App.Application();
+        /**
+         * Calls startup methods and dependencies in the Application
+         */
+        mainApp.bootstrap();
 
-    var t1 = performance.now();
-    console.log('Boostrap End at '+t1+'ms');
-    console.log('Boostrap took '+(t1 - t0)+' milliseconds');
+        var t3 = performance.now();
+        console.log('Boostrap End at '+t3+'ms');
+        console.log('Boostrap took '+(t3 - t2)+' milliseconds');
+
+    }, 500);
+
 });
---
+++ ts/module/Authentication.ts
@@ -0,0 +1,12 @@
+/// <reference path="../core/Module.ts" />
+
+module App {
+    export module Modules {
+        export class Authentication extends App.Module {
+
+            constructor() {
+                super();
+            }
+        }
+    }
+}
