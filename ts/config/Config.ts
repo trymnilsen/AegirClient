fefb2e5b5644c093903e6bfae09fba91a9f17b35
ts <trym.nilsen@gmail.com>
2015-07-08 20:32:26 +0200

    config file changes

--- ts/config/Config.ts
+++ ts/config/Config.ts
@@ -1,47 +1,27 @@
 module App {
 
-	export var config: {[id:string]: any} = {
+    export var config: {[id:string]: any} = {
         "UI"      : {
             "layoutContainer" : ".app-container"
         },
-		"Modules" : {
-			"PageRender" : {
-				"Pages"   : [
-		        	{
-		        		"id"		: "PG_HOME",
-		            	"name"      : "Home",
-		            	"routeName"	: "home",
-		            	"panels"    : [
-                             {
-                                    "type" : "TITLE_PANEL",
-                                    "name" : "News",
-                                    "columnsize" : "12",
-                                    "opts": {
-                                        "refresh":true,
-                                        "dismissable":false,
-                                        "dissmiss_time":"session",
-                                        "padd":true,
-                                    },
-                                    "components" : [
-                                        {
-                                            "id":"PL_NEWSPANEL",
-                                            "localConfig" : {
-
-                                            }
-                                        }
-                                    ]
-                             }
-                        ]
-		        	},
-		        	{
-		        		"id"		: "PG_LOCATION",
-		            	"name"      : "Location",
-		            	"routeName"	: "location",
-		            	"panels"    : {}
-		        	},
-	        	]
-			}
-		},
+        "Modules" : {
+            "PageRender" : {
+                "Pages"   : [
+                    {
+                        "id"        : "PG_HOME",
+                        "name"      : "Home",
+                        "routeName"    : "home",
+                        "panels"    : ["PL_NEWSPANEL"]
+                    },
+                    {
+                        "id"        : "PG_LOCATION",
+                        "name"      : "Location",
+                        "routeName"    : "location",
+                        "panels"    : ["PL_NEWSPANEL"]
+                    },
+                ]
+            }
+        },
         "Panels" : {
             "PL_NEWSPANEL" : {
                 "Configstuffhere":"Todo"
@@ -50,11 +30,6 @@ module App {
         "AppStates" : {
             "StartupState" : "AppReady",
             "States": {
-                "AuthState": {
-                    "ContainerClasses" : ["full-size-container"],
-                    "Events": ["Forbidden"],
-                    "Modules": ["Authentication"]
-                },
                 "AppReady": {
                     "ContainerClasses" : ["container"],
                     "Events": ["AuthSuccess", "AppBoot"],
@@ -62,5 +37,5 @@ module App {
                 }
             }
         }
-	}
+    }
 }
