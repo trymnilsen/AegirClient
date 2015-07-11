a36e9f23296e64e1745483b0ca36f396bd0e12b5
Trym Nilsen <trym.nilsen@gmail.com>
2015-07-11 13:28:22 +0200

    Navigation module tweaks

--- ts/module/Navigation.ts
+++ ts/module/Navigation.ts
@@ -10,6 +10,11 @@ module App {
              * navigate the app
              */
             private navBarView: App.View.Navigation.NavigationContainer = null;
+            /**
+             * Router instance this navigation uses
+             * @type {App.Core.Router}
+             */
+            private router: App.Core.Router;
 			constructor() {
 				super();
 			}
@@ -22,7 +27,8 @@ module App {
                     var buttonId      : string = <string>pages[i]['id'];
                     var buttonTitle   : string = <string>pages[i]['name'];
                     var buttonIcon    : string = <string>pages[i]['icon'];
-                    var button: App.View.Navigation.NavigationButton = new App.View.Navigation.NavigationButton(buttonId,buttonTitle,buttonIcon);
+                    var buttonRoute   : string = <string>pages[i]['routeName'];
+                    var button: App.View.Navigation.NavigationButton = new App.View.Navigation.NavigationButton(buttonId,buttonTitle,buttonIcon,buttonRoute);
                     buttons.push(button);
                 }
                 this.navBarView = new App.View.Navigation.NavigationContainer(buttons);
@@ -30,6 +36,44 @@ module App {
                 //Append it using its attachnode
                 App.AppView.resolveViewAppendPoint(this.navBarView)
                                         .append(this.navBarView.$el);
+
+                this.createRouter();
+            }
+            /**
+             * Create the router instance
+             */
+            private createRouter(): void {
+                //Create router instance
+                this.router = new App.Core.Router();
+                //Catch all
+                this.router.route("*actions",
+                                  "defaultRoute",
+                                  _.bind(this.defaultRoute, this));
+                //Store all routes
+                var pages : Array<Object> = App.config["Modules"]
+                                                      ["PageRender"]
+                                                      ["Pages"];
+
+                for(var i = 0; i<pages.length; i++)
+                { 
+                    var route       : string = pages[i]["routeName"];
+                    var name        : string = pages[i]["name"];
+                    this.router.route(route+"(/*params)",
+                        name,_.bind(this.routeChanged, this, route));
+                }
+
+                this.router.initialize();
+                Backbone.history.start();
+            }
+            private defaultRoute(attempedRoute: any): void
+            {
+                console.log("[NAVIGATION:routing]DefaultRoute",attempedRoute);
+            }
+            private routeChanged(pageId: string, params: string):void {
+                //The params is delivered on the format /xxx/yyy/zzz,
+                //I.E just the last part of the url.
+                console.log(pageId,params);
+
             }
 		}
 	}
