a47cf25b2891730e2800af430b500de8be779982
Trym Nilsen <trym.nilsen@gmail.com>
2015-07-14 15:26:26 +0200

    Replay contex skeleton

---
+++ ts/context/ReplayContext.ts
@@ -0,0 +1,14 @@
+/// <reference path="../core/Context.ts" />
+
+module App.Context {
+    export class ReplayContext extends App.Core.Context {
+
+        constructor() {
+            super();
+        }
+        public ReplayData(bowFile: string, sternFile: string, vesselConfig: any)
+        {
+        
+        }
+    }
+}
