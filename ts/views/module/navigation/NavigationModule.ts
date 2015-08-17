f1e82b36dbb38b88fa71ea5b1696619904d95e58
Trym Nilsen <trym.nilsen@gmail.com>
2015-08-17 23:02:33 +0200

    navigation module

--- ts/views/module/navigation/NavigationModule.ts
+++ ts/views/module/navigation/NavigationModule.ts
@@ -1,6 +1,7 @@
 /// <reference path="../BaseModule.ts" />
 /// <reference path="../../layout/ELayoutPosition.ts" />
 /// <reference path="NavigationModule.html.ts" />
+/// <reference path="NavigationVisualization.ts" />
 
 
 module App.View.Mod {
@@ -9,6 +10,12 @@ module App.View.Mod {
          constructor(){
              super(App.View.Layout.ELayoutPosition.LEFTCENTER, "Navigation");
              this.setTemplate(App.Template.NavigationModule.html);
+
+             let navCanvas: App.View.Mod.Navigation.NavigationVisualization = new App.View.Mod.Navigation.NavigationVisualization();
+             navCanvas.appendOptions = {
+                 AttachPointSelector : ".nav-viz-wrapper"
+             };
+             this.appendView(navCanvas);
          }
     }
 }
--- ts/views/module/navigation/NavigationVisualization.ts
+++ ts/views/module/navigation/NavigationVisualization.ts
@@ -2,7 +2,9 @@
 
 module App.View.Mod.Navigation {
 
-    export class PlaybackView extends App.View.AppView {
+    export class NavigationVisualization extends App.View.AppView {
+
+        private ctx: CanvasRenderingContext2D;
 
         /**
          * Instantiates a new instance of the menu bar
@@ -11,8 +13,42 @@ module App.View.Mod.Navigation {
             //Init parent
             super({backboneOptions : {
                 className: "nav-viz-canvas",
-                tagName: "canvas"
+                tagName: "canvas",
+                attributes: {
+                    width: "250px",
+                    height: "350px"
+                }
             }});
+
+            this.ctx = this.el.getContext('2d');
+
+            this.on("mousedown", this.mouseDown, this);
+            this.on("mouseup", this.mouseUp, this);
         }
+        private mouseDown(event): void {
+            console.log(event);
+        }
+        private mouseUp(event): void {
+            console.log(event);
+        }
+        private drawShip(): void {
+
+        }
+        private drawGrid(): void {
+            for (var x = 0; x <= 250; x += 40) {
+                this.ctx.moveTo(0.5 + x, 0);
+                this.ctx.lineTo(0.5 + x, 350);
+            }
+
+
+            for (var x = 0; x <= 350; x += 40) {
+                this.ctx.moveTo(0, 0.5 + x );
+                this.ctx.lineTo(250, 0.5 + x);
+            }
+
+            this.ctx.strokeStyle = "black";
+            this.ctx.stroke();
+        }
+
     }
 }
