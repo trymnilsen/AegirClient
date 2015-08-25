8dd19ab9dae87ae4cc67262b3be1f549fddbae99
Trym Nilsen <trym.nilsen@gmail.com>
2015-08-25 17:02:18 +0200

    added project model

---
+++ ts/data/model/project/Project.ts
@@ -0,0 +1,15 @@
+/// <reference path="../../../typings/backbone.d.ts" />
+
+module App.Data.Model.Project {
+
+    export class Project extends Backbone.Model{
+        /**
+         * Contains data about a single search result
+         */
+        constructor() {
+            //Init parent
+            super({});
+
+        }
+    }
+}
